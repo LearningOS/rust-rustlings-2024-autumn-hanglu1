a7566b6955b2b95a63f57569ada816abe086ca2b
luhang <1617045602@qq.com>
2024-10-20 14:05:53 +0800

    solve smart_pointers,threads,macros

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-macro_rules! my_macro {
-    () => {
-        println!("Check out my macro!");
+
+macro_rules! greeting {
+    ($name:expr) => {
+        println!("Hello, {}!", $name);
     };
 }
 
 fn main() {
-    my_macro();
+    greeting!("Rustacean");
 }
--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,22 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[rustfmt::skip]
 macro_rules! my_macro {
+    // The first rule for when no argument is passed
     () => {
         println!("Check out my macro!");
-    }
+    };
+    // The second rule for when an argument ($val:expr) is passed
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
 }
 
 fn main() {
+    // Call the macro without an argument
     my_macro!();
+    // Call the macro with an argument 7777
     my_macro!(7777);
 }
